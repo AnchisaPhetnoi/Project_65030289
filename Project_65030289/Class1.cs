a7c5789e0b5ff2831364f56d5561573fc8f8a0b5
AnchisaPhetnoi <65030289@kmitl.ac.th>
2023-10-18 11:43:35 +0700

    Add project files.

---
+++ Project_65030289/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_65030289
+{
+    public class Project
+    {
+        public enum Members
+        {
+            Toey = 1,
+            Khim = 2,
+            Woun = 3,
+
+        }
+    }
+}
+
+    
